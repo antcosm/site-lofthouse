5a6be7eb12df50096e47e6a9acba4cb4df2042c6
antcosm <85739526+antcosm@users.noreply.github.com>
2024-02-06 15:21:18 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* Nav icon */
 const navBtn = document.querySelector('.nav-icon-btn');
 const navIcon = document.querySelector('.nav-icon');
 const nav = document.querySelector('.header__top-row');
@@ -22,53 +23,55 @@ phoneInputs.forEach((input)=>{
 	})
 });
 
-/*Yandex map*/
+/* Yandex Map */
 
-  // Функция ymaps.ready() будет вызвана, когда
-  // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
 ymaps.ready(init);
 function init(){
-  // Создание карты.
-  var map = new ymaps.Map("map", {
-      // Координаты центра карты.
-      // Порядок по умолчанию: «широта, долгота».
-      // Чтобы не определять координаты центра карты вручную,
-      // воспользуйтесь инструментом Определение координат.
-      center: [59.943543, 30.338928],
-      // Уровень масштабирования. Допустимые значения:
-      // от 0 (весь мир) до 19.
-      zoom: 16,
-  });     	
+	// Создание карты.
+	var map = new ymaps.Map('map', {
+		// Координаты центра карты.
+		// Порядок по умолчанию: «широта, долгота».
+		// Чтобы не определять координаты центра карты вручную,
+		// воспользуйтесь инструментом Определение координат.
+		center: [59.943543, 30.338928],
+		// Уровень масштабирования. Допустимые значения:
+		// от 0 (весь мир) до 19.
+		zoom: 16,
+	});
 
-  var myPlacemark = new ymaps.Placemark([59.943543, 30.338928],
-    {
-      balloonContent: `
-        <div class="balloon">
-          <div class="balloon__address">Наб. реки Фонтанки 10-15</div>
-          <div class="balloon__contacts">
-            <a href="tel:+78121234567">+8 (812) 123-45-67</a>
-          </div>
-        </div>
-      `,
-      },
-    {
-      iconLayout: 'default#image',
-      iconImageHref: './img/map/location-pin.svg',
-      iconImageSize: [40, 40],
-      iconImageOffset: [-20, -40]
-    }
-  );
+	var myPlacemark = new ymaps.Placemark(
+		[59.943543, 30.338928],
+		{
+			balloonContent: `
+				<div class="balloon">
+					<div class="balloon__address">Наб. реки Фонтанки 10-15</div>
+					<div class="balloon__contacts">
+						<a href="tel:+78121234567">+8 (812) 123-45-67</a>
+					</div>
+				</div>
+			`,
+		},
+		{
+			iconLayout: 'default#image',
+			iconImageHref: './img/map/location-pin.svg',
+			iconImageSize: [40, 40],
+			iconImageOffset: [-20, -40],
+		}
+	);
 
-  map.controls.remove('geolocationControl'); // удаляем геолокацию
-  map.controls.remove('searchControl'); // удаляем поиск
-  map.controls.remove('trafficControl'); // удаляем контроль трафика
-  map.controls.remove('typeSelector'); // удаляем тип
+	map.controls.remove('geolocationControl'); // удаляем геолокацию
+	map.controls.remove('searchControl'); // удаляем поиск
+	map.controls.remove('trafficControl'); // удаляем контроль трафика
+	map.controls.remove('typeSelector'); // удаляем тип
 
-  // map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-  // map.controls.remove('zoomControl'); // удаляем контрол зуммирования
-  map.controls.remove('rulerControl'); // удаляем контрол правил
-  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+	// map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+	// map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+	map.controls.remove('rulerControl'); // удаляем контрол правил
+	map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
 
-  map.geoObjects.add(myPlacemark);
-  myPlacemark.balloon.open();  //изначальное открытие balloon//
+	map.geoObjects.add(myPlacemark);
+    myPlacemark.balloon.open();
+
 }
